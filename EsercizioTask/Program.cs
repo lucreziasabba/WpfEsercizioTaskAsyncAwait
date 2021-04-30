f208002f5f4788149c11e0a39131a356a7e4c1d7
marcopacini2002 <marco02pacio@gmail.com>
2021-04-30 08:44:43 +0200

    Add project files.

---
+++ EsercizioTask/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EsercizioTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task<int> t1 = Task.Factory.StartNew(() => StampaNumeri(true),
+                CancellationToken.None, 
+                TaskCreationOptions.LongRunning, 
+                TaskScheduler.Default
+                );
+
+            Task<int> t2 = Task.Factory.StartNew(() => StampaNumeri(false));
+
+            Console.WriteLine($"Totale numeri pari: {t1.Result}");
+            Console.WriteLine($"Totale numeri dispari: {t2.Result}");
+
+            Console.ReadLine();
+        }
+        private static int StampaNumeri(bool isPari)
+        {
+            int totaleNumeri = 0;
+            Console.WriteLine($"Il Task corrente usa i ThreadPool: {Thread.CurrentThread.IsThreadPoolThread}");
+            if (isPari)
+            {
+                for (int i = 0; i < 100; i = i + 2)
+                {
+                    Console.WriteLine($"Task n°{Task.CurrentId}: {i}");
+                    totaleNumeri++;
+                }
+            }
+            else
+            {
+                for (int i = 1; i < 100; i = i + 2)
+                {
+                    Console.WriteLine($"Task n°{Task.CurrentId}: {i}");
+                    totaleNumeri++;
+                }
+            }
+            Thread.Sleep(5000);
+            return totaleNumeri;
+
+        }
+    }
+    }
+}
